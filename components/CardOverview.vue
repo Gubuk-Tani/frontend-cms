acc734eb75d21055608fe5832f5c01b4831ae045
Sahid Anwar <hay.sahid@gmail.com>
2023-06-04 23:04:30 +0700

    feat: overview page

---
+++ components/CardOverview.vue
@@ -0,0 +1,116 @@
+<template>
+  <div
+    class="card form-container p-6 flex-grow flex-row gap-8 justify-between cursor-pointer"
+  >
+    <div class="flex flex-col gap-2">
+      <!-- Value -->
+      <div class="text-3xl text-primary-1">{{ value }}</div>
+
+      <!-- Headline -->
+      <div class="text-gray-600">{{ headline }}</div>
+    </div>
+
+    <!-- Icon -->
+    <div class="">
+      <!-- Product -->
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        class="w-[50px] h-[50px] stroke-primary-1 opacity-25 stroke-2"
+        v-if="type == 'product'"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
+        />
+      </svg>
+
+      <!-- Users -->
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        class="w-[50px] h-[50px] stroke-primary-1 opacity-25 stroke-2"
+        v-if="type == 'users'"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
+        />
+      </svg>
+
+      <!-- Articles -->
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        class="w-[50px] h-[50px] stroke-primary-1 opacity-25 stroke-2"
+        v-if="type == 'articles'"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+        />
+      </svg>
+
+      <!-- Diseases -->
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        class="w-[50px] h-[50px] stroke-primary-1 opacity-25 stroke-2"
+        v-if="type == 'diseases'"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12 12.75c1.148 0 2.278.08 3.383.237 1.037.146 1.866.966 1.866 2.013 0 3.728-2.35 6.75-5.25 6.75S6.75 18.728 6.75 15c0-1.046.83-1.867 1.866-2.013A24.204 24.204 0 0112 12.75zm0 0c2.883 0 5.647.508 8.207 1.44a23.91 23.91 0 01-1.152 6.06M12 12.75c-2.883 0-5.647.508-8.208 1.44.125 2.104.52 4.136 1.153 6.06M12 12.75a2.25 2.25 0 002.248-2.354M12 12.75a2.25 2.25 0 01-2.248-2.354M12 8.25c.995 0 1.971-.08 2.922-.236.403-.066.74-.358.795-.762a3.778 3.778 0 00-.399-2.25M12 8.25c-.995 0-1.97-.08-2.922-.236-.402-.066-.74-.358-.795-.762a3.734 3.734 0 01.4-2.253M12 8.25a2.25 2.25 0 00-2.248 2.146M12 8.25a2.25 2.25 0 012.248 2.146M8.683 5a6.032 6.032 0 01-1.155-1.002c.07-.63.27-1.222.574-1.747m.581 2.749A3.75 3.75 0 0115.318 5m0 0c.427-.283.815-.62 1.155-.999a4.471 4.471 0 00-.575-1.752M4.921 6a24.048 24.048 0 00-.392 3.314c1.668.546 3.416.914 5.223 1.082M19.08 6c.205 1.08.337 2.187.392 3.314a23.882 23.882 0 01-5.223 1.082"
+        />
+      </svg>
+
+      <!-- Detection Models -->
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        class="w-[50px] h-[50px] stroke-primary-1 opacity-25 stroke-2"
+        v-if="type == 'detection_models'"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
+        />
+      </svg>
+
+      <!-- Detections -->
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        class="w-[50px] h-[50px] stroke-primary-1 opacity-25 stroke-2"
+        v-if="type == 'detections'"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M7.5 3.75H6A2.25 2.25 0 003.75 6v1.5M16.5 3.75H18A2.25 2.25 0 0120.25 6v1.5m0 9V18A2.25 2.25 0 0118 20.25h-1.5m-9 0H6A2.25 2.25 0 013.75 18v-1.5M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+        />
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    headline: null,
+    value: null,
+    type: null,
+  },
+}
+</script>
--- pages/overview.vue
+++ pages/overview.vue
@@ -1,18 +1,322 @@
 <template>
-  <main
-    class="ml-[220px] max-lg:ml-[88px] max-md:ml-0 max-w-[100%] flex justify-center my-[60px] p-[24px] ease-in-out duration-200 max-md:flex-col max-md:mb-[88px]"
-  >
+  <!-- Main -->
+  <main class="main-dashboard">
+    <!-- Content -->
     <section
-      class="flex flex-row justify-between gap-6 flex-grow max-md:max-w-none items-start"
+      class="flex flex-row justify-between gap-6 flex-grow max-md:max-w-none"
     >
-      Overview Coming Soon...
+      <div class="flex flex-col gap-8 max-sm:gap-6 w-full">
+        <!-- Overview Headline -->
+        <div class="flex justify-between items-center gap-4">
+          <h1 class="font-bold text-2xl text-dark self-center py-1">
+            Overview
+          </h1>
+
+          <!-- Type -->
+          <select
+            class="input-field"
+            v-model="range"
+            @input="showByRange($event.target.value)"
+          >
+            <option value="">All</option>
+            <option value="1">Today</option>
+            <option value="7">Last 1 Week</option>
+            <option value="30">Last 1 Month</option>
+            <option value="365">Last 1 Year</option>
+          </select>
+        </div>
+
+        <!-- Summary -->
+        <div class="flex flex-col gap-4">
+          <!-- Cards -->
+          <div
+            class="flex flex-wrap flex-grow justify-between gap-4 flex-initial"
+          >
+            <!-- Product -->
+            <NuxtLink :to="{ name: 'users' }" class="w-[300px] flex-grow">
+              <CardOverview
+                headline="Users Created"
+                :value="overview.count_users"
+                type="users"
+              />
+            </NuxtLink>
+
+            <!-- Product -->
+            <NuxtLink :to="{ name: 'articles' }" class="w-[300px] flex-grow">
+              <CardOverview
+                headline="Articles Created"
+                :value="overview.count_articles"
+                type="articles"
+              />
+            </NuxtLink>
+
+            <!-- Articles -->
+            <NuxtLink :to="{ name: 'diseases' }" class="w-[300px] flex-grow">
+              <CardOverview
+                headline="Articles Created"
+                :value="overview.count_articles"
+                type="articles"
+              />
+            </NuxtLink>
+
+            <!-- Diseases -->
+            <NuxtLink :to="{ name: 'diseases' }" class="w-[300px] flex-grow">
+              <CardOverview
+                headline="Diseases Created"
+                :value="overview.count_diseases"
+                type="diseases"
+              />
+            </NuxtLink>
+
+            <!-- Diseases -->
+            <NuxtLink :to="{ name: 'detections' }" class="w-[300px] flex-grow">
+              <CardOverview
+                headline="Detection Models Created"
+                :value="overview.count_plants"
+                type="detection_models"
+              />
+            </NuxtLink>
+
+            <!-- Detections -->
+            <div class="w-[300px] flex-grow">
+              <CardOverview
+                headline="Detections Created"
+                :value="overview.count_detections"
+                type="detections"
+              />
+            </div>
+          </div>
+        </div>
+
+        <!-- Todays Detections Headline -->
+        <div class="flex justify-between items-center gap-4">
+          <h1 class="font-bold text-2xl text-dark self-center py-1">
+            Today's Detections
+          </h1>
+        </div>
+
+        <!-- Show Detections -->
+        <div class="relative overflow-x-auto h-[500px]">
+          <table class="w-full text-sm text-left text-gray-500">
+            <thead
+              class="text-xs text-gray-700 uppercase bg-gray-100 sticky inset-0"
+            >
+              <tr>
+                <th scope="col" class="px-6 py-3">Detection</th>
+                <th scope="col" class="px-6 py-3">Result</th>
+                <th scope="col" class="px-6 py-3">Disease</th>
+              </tr>
+            </thead>
+            <tbody class="overflow-y-auto">
+              <tr
+                class="bg-white border-b hover:bg-gray-50"
+                v-for="(item, index) in listDetection"
+                :key="item.detection.id"
+              >
+                <!-- Image -->
+                <th scope="row" class="px-6 py-4 text-gray-900">
+                  <div class="flex gap-4 items-center">
+                    <!-- Image -->
+                    <img
+                      :src="
+                        item.detection.image
+                          ? imgUrl + item.detection.image
+                          : ''
+                      "
+                      alt=""
+                      class="aspect-video max-sm:w-[80px] h-[80px] object-cover rounded-md"
+                      v-if="item.detection.image"
+                    />
+
+                    <div class="flex flex-col gap-1.5">
+                      <!-- Plant Name -->
+                      <div class="text-base font-semibold">
+                        {{ item.detection.plant.name }}
+                      </div>
+
+                      <!-- Created At -->
+                      <div class="flex gap-1.5 items-center">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          class="w-5 h-5 fill-gray-400 max-sm:hidden"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm.75-13a.75.75 0 00-1.5 0v5c0 .414.336.75.75.75h4a.75.75 0 000-1.5h-3.25V5z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+
+                        <div
+                          class="font-normal text-gray-500 flex flex-wrap gap-x-1.5"
+                        >
+                          <div class="whitespace-nowrap">
+                            {{ item.detection.created_at.split(' ')[0] }}
+                          </div>
+                          <div>
+                            {{ item.detection.created_at.split(' ')[1] }}
+                          </div>
+                        </div>
+                      </div>
+
+                      <!-- Username -->
+                      <div
+                        class="font-normal text-gray-500 flex gap-1.5 items-center"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          class="w-5 h-5 fill-gray-400 max-sm:hidden"
+                        >
+                          <path
+                            d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z"
+                          />
+                        </svg>
+                        <div>{{ item.detection.user.username }}</div>
+                      </div>
+                    </div>
+                  </div>
+                </th>
+
+                <!-- Result -->
+                <td class="px-6 py-4">
+                  <div class="flex flex-col gap-1">
+                    <div
+                      :class="
+                        item.detection.result == 'error'
+                          ? 'text-red-600'
+                          : 'text-green-600'
+                      "
+                    >
+                      {{ item.detection.result }}
+                    </div>
+                    <div class="">{{ item.detection.confidence }}</div>
+                  </div>
+                </td>
+
+                <!-- Linked Disease -->
+                <td class="px-6 py-4">
+                  <!-- Linked Disease -->
+                  <div class="flex flex-col gap-1" v-if="item.disease">
+                    <div
+                      class="flex gap-2 p-2 items-center border border-gray-200 rounded-md hover:border-gray-300"
+                    >
+                      <!-- Images -->
+                      <img
+                        :src="
+                          item.disease.image ? imgUrl + item.disease.image : ''
+                        "
+                        alt=""
+                        class="w-[40px] h-[40px] object-cover rounded-sm max-sm:hidden"
+                        v-if="item.disease.image"
+                      />
+
+                      <div
+                        class="w-[40px] h-[40px] flex justify-center items-center bg-gray-200 rounded-sm"
+                        v-else
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          class="w-14 h-14 fill-gray-400"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+                      </div>
+
+                      <!-- Title -->
+                      <div
+                        class="text-md text-gray-900 font-medium line-clamp-2 w-full"
+                      >
+                        {{ item.disease.name }}
+                      </div>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </section>
   </main>
 </template>
 
 <script>
 export default {
-  layout: 'LayoutDashboard',
   middleware: 'admin',
+  layout: 'LayoutDashboard',
+  data() {
+    return {
+      imgUrl: process.env.imgUrl,
+      overview: [],
+      detections: [],
+
+      DetectionWithDisease: [],
+
+      // Options
+      range: '',
+
+      query: {
+        range: '',
+      },
+    }
+  },
+  async fetch() {
+    // Get Overview
+    const resOverview = await this.$axios.get('/api/overview', {
+      params: {
+        range: this.range,
+      },
+    })
+
+    this.overview = resOverview.data.result
+
+    // Get Detections
+    const resDetections = await this.$axios.get('/api/detection', {
+      params: {
+        range: 'today',
+      },
+    })
+
+    this.detections = resDetections.data.result.data
+
+    // Linked Disease
+    for (let i = 0; i < this.detections.length; i++) {
+      const resLinkedDisease = await this.$axios.get('/api/disease', {
+        params: {
+          tag: this.detections[i].result,
+        },
+      })
+
+      const item = {
+        detection: this.detections[i],
+        disease: resLinkedDisease.data.result.disease,
+      }
+
+      this.DetectionWithDisease.push(item)
+    }
+  },
+  computed: {
+    listDetection() {
+      return this.DetectionWithDisease
+    },
+  },
+  methods: {
+    // Show by Range
+    showByRange(range) {
+      this.range = range
+
+      this.$fetch()
+    },
+  },
 }
 </script>
